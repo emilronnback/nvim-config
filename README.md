ed1918463611d731a294222be508aca972fa0b47
Emil Rönnbäck <emilronnback@proton.me>
2024-03-13 07:03:46 +0100

    Add config files

---
+++ README.md
@@ -0,0 +1,1 @@
+Neovim cofig using lazyvim
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,90 @@
+return {
+  -- Active indent guide and indent text objects. When you're browsing
+  -- code, this highlights the current level of indentation, and animates
+  -- the highlighting.
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      draw = {
+        animation = require("mini.indentscope").gen_animation.none(),
+        delay = 0,
+      },
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+  --{
+  --  "goolord/alpha-nvim",
+  --  event = "VimEnter",
+  --  opts = function()
+  --    local dashboard = require("alpha.themes.dashboard")
+  --    local logo = [[]]
+  --    dashboard.section.header.val = vim.split(logo, "\n")
+  --    dashboard.section.buttons.val = {
+  --      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+  --      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+  --      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+  --      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+  --      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+  --      dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+  --      dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+  --      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+  --    }
+  --    for _, button in ipairs(dashboard.section.buttons.val) do
+  --      button.opts.hl = "AlphaButtons"
+  --      button.opts.hl_shortcut = "AlphaShortcut"
+  --    end
+  --    dashboard.section.header.opts.hl = "AlphaHeader"
+  --    dashboard.section.buttons.opts.hl = "AlphaButtons"
+  --    dashboard.section.footer.opts.hl = "AlphaFooter"
+  --    dashboard.opts.layout[1].val = 8
+  --    return dashboard
+  --  end,
+  --  config = function(_, dashboard)
+  --    -- close Lazy and re-open when the dashboard is ready
+  --    if vim.o.filetype == "lazy" then
+  --      vim.cmd.close()
+  --      vim.api.nvim_create_autocmd("User", {
+  --        pattern = "AlphaReady",
+  --        callback = function()
+  --          require("lazy").show()
+  --        end,
+  --      })
+  --    end
+
+  --    require("alpha").setup(dashboard.opts)
+
+  --    vim.api.nvim_create_autocmd("User", {
+  --      pattern = "LazyVimStarted",
+  --      callback = function()
+  --        local stats = require("lazy").stats()
+  --        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+  --        dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+  --        pcall(vim.cmd.AlphaRedraw)
+  --      end,
+  --    })
+  --  end,
+  --},
+}
